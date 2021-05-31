2d2a39cdac6a124cfa00617d8df30ea7a05c2df0
Pedro Henrique Abreu <pedroabreu.dev@gmail.com>
2021-05-31 11:40:00 -0400

    Inserindo o List View

--- app/src/main/java/br/com/diastecnologia/agenda_alura/MainActivity.java
+++ app/src/main/java/br/com/diastecnologia/agenda_alura/MainActivity.java
@@ -1,7 +1,33 @@
 package br.com.diastecnologia.agenda_alura;
 
 import android.app.Activity;
+import android.os.Bundle;
+
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import android.widget.ListView;
+
+import androidx.annotation.Nullable;
 
 public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        List<String> alunos = new ArrayList<>(
+                Arrays.asList("Pedro", "Fran", "Jose", "Maria", "Ana"));
+        ListView listaDeAlunos = findViewById(R.id.activity_main_lista_de_alunos);
+        listaDeAlunos.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, alunos));
+
+
+
+    }
 
 }
